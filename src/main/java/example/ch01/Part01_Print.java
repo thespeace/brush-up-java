124621b2a6ffaa8a37c95e560bd501c062c20490
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2023-04-05 23:59:26 +0900

    230405 23:59 regular commit.

--- src/main/java/example/ch01/Part01_Print.java
+++ src/main/java/example/ch01/Part01_Print.java
@@ -3,7 +3,7 @@ package example.ch01;
 import java.io.IOException;
 
 public class Part01_Print {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         // System.out : *표준 출력 스트림*으로 전달 받은 데이터를 스트림을 통해 출력 장치로 전달되어 사용자에게 출력.
         // print()    : 출력 후, 줄바꿈 안함.
         // println()  : 출력 후, 줄바꿈.
