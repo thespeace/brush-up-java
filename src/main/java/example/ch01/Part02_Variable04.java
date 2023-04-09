6444711b4142bbd0747b89e4581447b78c8ec1a1
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2023-04-09 22:03:48 +0900

    230409 22:03 regular commit.

---
+++ src/main/java/example/ch01/Part02_Variable04.java
@@ -0,0 +1,11 @@
+package example.ch01;
+
+public class Part02_Variable04 {
+    public static void main(String[] args){
+        char ch = 'A'; // 하나의 문자를 '(작은 따옴표)를 사용해 선언 및 초기화.
+        String s1 = "ABC"; // 문자열 리터럴 : 하나 이상의 문자를 "(큰 따옴표)를 사용해 String 타입 선언 및 초기화.
+        String s2 = new String("AB"); // 원래 클래스는 new 연산자를 써서 객체를 생성해야 하는데,
+                                             // 워낙 자주사용하기 때문에 String 클래스만 특별히 기본형 변수에 값을 저장하는 것과 같은 표현을 허용합니다.
+
+    }
+}
