63621f8bb2d3229ad37f2d0504905a70a9dd2b62
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2023-04-10 23:04:33 +0900

    230410 23:03 Learn Replace values for two variables.

---
+++ src/main/java/example/ch01/Part02_Variable05.java
@@ -0,0 +1,23 @@
+package example.ch01;
+
+public class Part02_Variable05 {
+    public static void main(String[] args){
+        /* *
+         *    두 변수의 값 교환하기.
+         **/
+
+        int x1 = 10, y1 = 20;
+        x1 = y1; //y의 값을 x에 저장, x는 y로 대체.
+        System.out.println("x1 : " + x1 + ", y1 : " + y1);
+        y1 = x1; // x의 값을 y에 저장, y는 x로 대체.
+        System.out.println("x1 : " + x1 + ", y1 : " + y1); //결국 두 값은 같아져 버렸습니다.
+
+        int x2 = 30, y2 = 40;
+        int tmp; //빈 값을 가진 변수.
+        tmp = x2; //x2의 값을 tmp에 저장.
+        x2 = y2; //x2의 값을 y2에 저장.
+        y2 = tmp; //tmp의 값을 y2에 저장.
+        System.out.println("x2 : " + x2 + ", y2 : " + y2); //두 값을 바꾸었습니다.
+
+    }
+}
