82670b0ca6dbc495325101668d2786b20ad095e0
thespeace <thespeace1@gmail.com>
2023-04-25 19:17:26 +0900

    230425 19:17 regular commit.

---
+++ src/main/java/example/ch04/Conditional04.java
@@ -0,0 +1,15 @@
+package example.ch04;
+import java.util.Scanner;
+
+public class Conditional04 {
+    public static void main(String[] args){
+        /**
+         *    - 난수(임의의 정수 || 실수) 만들기
+         *      " 0.0 <= Math.random() < 1.0 "  : 0.0 ~ 0.99999...
+         *      Math.random() - 0.0과 1.0사이의 임의의 double 값을 반환.
+         */
+
+        System.out.println();
+
+    }
+}
