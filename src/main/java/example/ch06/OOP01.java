e71c63b0e17cafca5b6ae3d07d47b17b92ec3ad2
thespeace <thespeace1@gmail.com>
2023-05-04 17:20:46 +0900

    230504 17:20 a new chapter, regular commit.

---
+++ src/main/java/example/ch06/OOP01.java
@@ -0,0 +1,11 @@
+package example.ch06;
+
+public class OOP01 {
+    public static void main(String[] args){
+        /**
+         *    - 객체지향이란?
+         *      객체 지향 프로그래밍(영어: Object-Oriented Programming, OOP)은 컴퓨터 프로그래밍의 패러다임 중 하나이다.
+         */
+
+    }
+}
