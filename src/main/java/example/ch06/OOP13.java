8f021bf23c8203c29fab011aa91f83a090124269
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2023-05-11 22:16:56 +0900

    230511 22:16 Learn Basic Type Parameters.

--- src/main/java/example/ch06/OOP13.java
+++ src/main/java/example/ch06/OOP13.java
@@ -6,8 +6,6 @@ public class OOP13 {
          *    - 기본형 매개 변수
          *      1. 기본형 매개변수 : 변수의 값을 읽기만 할 수 있다.(read only)
          *      2. 참조형 매개변수 : 변수의 값을 읽고 변경할 수 있다.(read & write)
-         *
-         *
          */
 
         Data d = new Data(); //객체 생성
@@ -17,7 +15,8 @@ public class OOP13 {
         change(d.x);
         System.out.println("After change(d.x)");
         System.out.println("main() : x = " + d.x);
-        System.out.println("change메서드에서 지역변수로서 매개변수가 읽히고 해당 메서드를 수행한 뒤 사라진다... 그리고 ");
+        System.out.println("change메서드에서는 매개변수가 지역변수로서 읽히고 해당 메서드를 수행한 뒤 사라진다." +
+                           "\n기본형 매개변수를 사용하면 이처럼 값을 바꿀 순 없고 읽고 지역변수로서 읽고 사용 할 수만 있다.");
 
     }
     static void change(int x){ // 기본형 매개변수
