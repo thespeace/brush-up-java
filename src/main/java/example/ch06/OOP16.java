bc50e596e5aef780594232d9e5e1470ae96c5334
thespeace <thespeace1@gmail.com>
2023-05-12 18:26:40 +0900

    230512 18:26 regular commit.

---
+++ src/main/java/example/ch06/OOP16.java
@@ -0,0 +1,12 @@
+package example.ch06;
+
+public class OOP16 {
+    public static void main(String[] args){
+        /**
+         *    - 생성자(constructor)
+         *      인스턴스가 생성될 때마다 호출되는 '인스턴스 초기화 메서드'
+         */
+
+
+    }
+}
