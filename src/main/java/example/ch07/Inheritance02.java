7f1ff02d83e8f5422da12d4c733c23370d734753
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2023-05-16 21:13:31 +0900

    230516 21:13 regular commit.

---
+++ src/main/java/example/ch07/Inheritance02.java
@@ -0,0 +1,19 @@
+package example.ch07;
+
+public class Inheritance02 {
+    public static void main(String[] args){
+        /**
+         *    - 포함(Composite)이란?
+         *      클래스의 멤버로 참조변수를 선언하는 것.
+         *
+         *    - 포함 관계(클래스들의 관계)
+         *      크게 두가지로 나눌수 있다 : 1.상속  2.포함
+         *
+         *
+         *
+         *
+         *
+         */
+
+    }
+}
