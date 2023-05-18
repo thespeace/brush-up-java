5a58c563a14541bad5eb7d5017f6da10699d82c8
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2023-05-18 23:30:35 +0900

    230518 23:30 regular commit

---
+++ src/main/java/example/ch07/Inheritance05.java
@@ -0,0 +1,13 @@
+package example.ch07;
+
+public class Inheritance05 {
+    public static void main(String[] args){
+        /**
+         *    - 참조변수 super
+         *      객체 자신을 가리키는 참조변수. 인스턴스 메서드(생성자)내에만 존재
+         *      조상의 멤버를 자신의 멤버와 구별할 때 사용
+         */
+
+
+    }
+}
