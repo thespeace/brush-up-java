cc8b1dab25815b62c480a6b5d63fd0bd2d23d0a8
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2023-05-20 16:12:45 +0900

    230520 16:12 regular commit.

---
+++ src/main/java/example/ch07/Inheritance08.java
@@ -0,0 +1,18 @@
+package example.ch07;
+
+public class Inheritance08 {
+    public static void main(String[] args){
+        /**
+         *    - 제어자(modifier) == 형용사(명사 앞에 붙어서 명사를 꾸며줌)
+         *      클래스와 클래스의 멤버(멤버 변수, 메서드)에 부가적인 의미 부여.
+         *
+         *          1. 접근제어자 : public, protected, (default), private -> 단 하나만 명시 가능.
+         *          2. 그     외 : static, final, abstract, native, transient, synchronized, volatile, strictfp
+         *      
+         *      하나의 대상에 여러 제어자를 같이 사용가능(접근 제어자는 하나만)
+         *          ex) public ... 계속
+         *
+         */
+
+    }
+}
