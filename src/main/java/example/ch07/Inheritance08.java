f54bff8ec2830ffbbc51021cf59b6028de6c9c18
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2023-05-21 21:50:11 +0900

    230521 21:50 Learn static.

--- src/main/java/example/ch07/Inheritance08.java
+++ src/main/java/example/ch07/Inheritance08.java
@@ -10,9 +10,30 @@ public class Inheritance08 {
          *          2. 그     외 : static, final, abstract, native, transient, synchronized, volatile, strictfp
          *      
          *      하나의 대상에 여러 제어자를 같이 사용가능(접근 제어자는 하나만)
-         *          ex) public ... 계속
+         *          ex) public static final int WIDTH = 200;
+         *
+         *    - static : 클래스의, 공통적인
+         *      1. 멤버변수 : 모든 인스턴스에 공통적으로 사용되는 클래스 변수가 된다.
+         *                   클래스 변수는 인스턴스를 생성하지 않고도 사용 가능하다.
+         *                   클래스가 메모리에 로드될 때 생성된다.
+         *      2. 메서드  : 인스턴스를 생성하지 않고도 호출이 가능한 static 메서드가 된다.
+         *                  static 메서드 내에서는 객체가 필요한 인스턴스 멤버들을 직접 사용할 수 없다.
          *
          */
 
+    }
+}
+
+//static 사용 예
+class StaticTest {
+    static int width = 200;  // 클래스변수(static 변수), 간단 초기화(명시적 초기화).
+    static int height = 120; // 클래스변수(static 변수).
+
+    static { // 클래스 초기화 블럭
+        // static 변수의 복잡한 초기화 수행
+    }
+
+    static int max(int a, int b) { // 클래스 메서드(static메서드)
+        return a > b ? a : b; // 인스턴스 변수/메서드 사용 불가.
     }
 }
