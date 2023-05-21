625a931c12f3ca9a15b4ed80b6de3a886c7a6d86
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2023-05-21 22:10:19 +0900

    230521 22:10 regular commit.

---
+++ src/main/java/example/ch07/Inheritance09.java
@@ -0,0 +1,16 @@
+package example.ch07;
+
+public class Inheritance09 {
+    public static void main(String[] args){
+        /**
+         *    - 접근 제어자(access modifier)
+         *      1. private   : 같은 클래스 내에서만 접근이 가능하다.                               ( 접근 가능 : 같은 클래스 )
+         *      2. (default) : 같은 패키지 내에서만 접근이 가능하다.                               ( 접근 가능 : 같은 클래스, 같은 패키지)
+         *      3. protected : 같은 패키지 내에서, 그리고 다른 패키지의 자손클래스에서 접근이 가능하다.  ( 접근 가능 : 같은 클래스, 같은 패키지, 자손클래스(다른 패키지) )
+         *      4. public    : 접근 제한이 전혀 없다.                                            ( 접근 가능 : 같은 클래스, 같은 패키지, 자손클래스(다른 패키지), 전체 )
+         *
+         *      접근 범위 : public > protected > (default) > private
+         */
+
+    }
+}
