fc3e233dbede24e3b6cfd83a6408b3942be92ab3
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2023-05-22 22:39:55 +0900

    230522 22:39 regular commit.

---
+++ src/main/java/example/ch07/Inheritance10.java
@@ -0,0 +1,12 @@
+package example.ch07;
+
+public class Inheritance10 {
+    public static void main(String[] args){
+        /**
+         *    - 캡슐화와 접근 제어자
+         *      Q. 접근 제어자를 사용하는 이유?
+         *         외부로부터 데이터를 보호하기 위해서.(캡슐화)
+         */
+
+    }
+}
