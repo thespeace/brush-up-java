7224c20c49f230ae4e9abe327df4ac415e43f581
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2023-05-26 23:44:35 +0900

    230526 23:44 regular commit.

---
+++ src/main/java/example/ch07/Inheritance15.java
@@ -0,0 +1,11 @@
+package example.ch07;
+
+public class Inheritance15 {
+    public static void main(String[] args){
+        /**
+         *    - 여러 종류의 객체를 배열로 다루기.
+         *      조상타입의 배열에 자손들의 객체를 담을 수 있다.
+         */
+
+    }
+}
