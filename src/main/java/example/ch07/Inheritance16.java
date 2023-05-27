ad4ab495e15d2da3cb0dc4bdc92cb69b2e8d8648
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2023-05-27 23:09:26 +0900

    230527 23:09 regular commit.

---
+++ src/main/java/example/ch07/Inheritance16.java
@@ -0,0 +1,11 @@
+package example.ch07;
+
+public class Inheritance16 {
+    public static void main(String[] args){
+        /**
+         *    - 추상 클래스(abstract class)
+         *      미완성 설계도, 미완성 메서드를 갖고 있는 클래스
+         */
+
+    }
+}
