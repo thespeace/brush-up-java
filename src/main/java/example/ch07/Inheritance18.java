3b69458e5be4250fa4419358172613b4aa232379
thespeace <thespeace1@gmail.com>
2023-05-30 19:22:41 +0900

    230530 19:22 regular commit.

--- src/main/java/example/ch07/Inheritance18.java
+++ src/main/java/example/ch07/Inheritance18.java
@@ -2,5 +2,19 @@ package example.ch07;
 
 public class Inheritance18 {
     public static void main(String[] args){
+        /**
+         *    - 인터페이스(interface)
+         *      "추상 메서드의 집합" ( 프로그래밍 관점 ).
+         *      구현된 것이 전혀 없는 설계도, 즉 껍데기(모든 멤버가 public).
+         *      인스턴스 변수를 가질 수 없다.
+         *
+         *      Q.추상클래스와 인터페이스의 차이점은?
+         *      A.추상클래스는 일반 클래스인데 추상 메서드를 가지고 있어서 추상클래스가 되지만, 인터페이스는 추상 메서드만 가지고 있는 것.
+         *
+         *          ex) interface 인터페이스이름 {
+         *                  public...
+         *              }
+         */
+
     }
 }
