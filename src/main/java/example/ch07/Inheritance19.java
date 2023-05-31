1d44201fc618ca243f887a6716b0215c74673f31
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2023-05-31 22:47:40 +0900

    230531 22:47 regular commit.

---
+++ src/main/java/example/ch07/Inheritance19.java
@@ -0,0 +1,11 @@
+package example.ch07;
+
+public class Inheritance19 {
+    public static void main(String[] args){
+        /**
+         *    - 인터페이스를 이용한 다형성
+         *      인터페이스도 구현 클래스의 부모?
+         */
+
+    }
+}
