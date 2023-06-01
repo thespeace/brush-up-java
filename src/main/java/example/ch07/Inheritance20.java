b07b824fe20a8c5e1680043cb41deb0199175866
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2023-06-01 23:22:11 +0900

    230601 23:22 regular commit.

---
+++ src/main/java/example/ch07/Inheritance20.java
@@ -0,0 +1,14 @@
+package example.ch07;
+
+public class Inheritance20 {
+    public static void main(String[] args){
+        /**
+         *    - 인터페이스의 개념과 장점
+         *      interface == inter(~사이) + face(얼굴, 대상).
+         *      1.두 대상(객체) 간의 '연결, 대화, 소통'을 돕는 '중간 역할'을 한다.
+         *          혹은 '껍데기, 기계의 껍데기'로 복잡한 기계를 인간이 다루기 쉽게 가이드라인을 제공하는 역할을 하는 것을 인터페이스라 한다. ex) 자판기의 외형, 컴퓨터의 GUI.
+         *      2.선언(설계)와 구현을 분리시킬 수 있게 한다.
+         */
+
+    }
+}
