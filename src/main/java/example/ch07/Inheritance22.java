c9defb1d172f8b48111830da41e8014f56f75868
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2023-06-03 23:40:02 +0900

    230603 23:39 regular commit.

---
+++ src/main/java/example/ch07/Inheritance22.java
@@ -0,0 +1,19 @@
+package example.ch07;
+
+public class Inheritance22 {
+    public static void main(String[] args){
+        /**
+         *    - 내부 클래스(inner class)
+         *      클래스 안의 클래스
+         *          ex) class A { // 외부 클래스
+         *                  ...
+         *                  class B { // 내부 클래스
+         *                      ...
+         *                  }
+         *                  ...
+         *              }
+         *              
+         *
+         */
+    }
+}
