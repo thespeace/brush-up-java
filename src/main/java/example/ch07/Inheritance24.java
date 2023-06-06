7db59beecb531ddcafe47a8d9fa15308768ca210
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2023-06-06 23:32:59 +0900

    230606 23:32 regular commit.

---
+++ src/main/java/example/ch07/Inheritance24.java
@@ -0,0 +1,20 @@
+package example.ch07;
+
+public class Inheritance24 {
+    public static void main(String[] args){
+        /**
+         *    - 익명 클래스(anonymous class)
+         *      이름이 없는 일회용 클래스. 정의와 생성을 동시에 수행하고 종료.
+         *
+         *      * 기본 문법 *
+         *          new 조상클래스이름() {
+         *              // 멤버 선언
+         *          }
+         *
+         *          new 구현인터페이스이름() {
+         *              // 멤버 선언
+         *          }
+         */
+
+    }
+}
