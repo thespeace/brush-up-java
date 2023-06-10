d725895cd24a92ee4c6273be62db97199ae53508
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2023-06-10 23:04:49 +0900

    230610 23:04 regular commit.

---
+++ src/main/java/example/ch08/Exception03.java
@@ -0,0 +1,11 @@
+package example.ch08;
+/**
+ *    - printStackTrace() 와 getMessage().
+ *      printStackTrace() : 예외발생 당시의 호출스택(Call Stack)에 있었던 메서드의 정보와 예외 메시지를 콘솔창에 출력한다.
+ *      getMessage() : 발생한 예외클래스의 인스턴스에 저장된 메시지를 얻을 수 있다.
+ */
+public class Exception03 {
+    public static void main(String[] args){
+
+    }
+}
