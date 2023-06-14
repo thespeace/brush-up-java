edf670ce41af6d709464dd30832843ba1e006124
thespeace <thespeace1@gmail.com>
2023-06-14 19:25:40 +0900

    230614 19:24 Learn Create custom exceptions, throw exceptions back.

--- src/main/java/example/ch08/Exception06.java
+++ src/main/java/example/ch08/Exception06.java
@@ -32,10 +32,24 @@ class MyException extends Exception { // Exception이기 때문에 필수처리 
 /**
  *    - 예외 되던지기(exception re-throwing)
  *      예외를 처리한 후에 다시 예외를 발생시키는 것
+ *      호출한 메서드와 호출된 메서드 양쪽 모두에서 예외처리하는 것
  */
 
 public class Exception06 {
     public static void main(String[] args){
+        try {
+            method1();
+        } catch (Exception e) { //5)예외 처리.
+            System.out.println("main메서드에서 예외가 처리되었습니다.");
+        }
+    }
 
+    static void method1() throws Exception { // 4)예외 선언, 호출한 쪽으로 보내기.
+        try {
+            throw new Exception(); // 1)예외 발생.
+        } catch (Exception e) {
+            System.out.println("method1메서드에서 예외가 처리되었습니다."); // 2)예외 처리.
+            throw e; // 3)다시 예외를 발생.
+        }
     }
 }
