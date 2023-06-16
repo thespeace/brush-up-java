2e0e7ed08c1191e6bdaa2b60c4c4a3656bfcfe28
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2023-06-16 23:57:24 +0900

    230616 23:57 regular commit.

---
+++ src/main/java/example/ch09/Class01.java
@@ -0,0 +1,12 @@
+package example.ch09;
+/**
+ *    - Object 클래스
+ *      모든 클래스의 최고 조상으로 오직 11개의 메서드만을 가지고 있다.
+ *      notify(), wait() 등은 쓰레드와 관련된 메서드입니다.
+ *
+ *          |       Object 클래스의 메서드        |                설   명               |
+ */
+public class Class01 {
+    public static void main(String[] args){
+    }
+}
