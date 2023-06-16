2f9eec7e28b053bffb47960915f6d588a3e80bd3
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2023-06-16 23:54:34 +0900

    230616 23:55 Learn Chained Exception.

--- src/main/java/example/ch08/Exception07.java
+++ src/main/java/example/ch08/Exception07.java
@@ -23,11 +23,44 @@ import java.io.Serializable;
  *          }
  *          ...
  *     }
+ *
+ *     void install() throws InstallException {
+ *     try {
+ *         startInstall(); // SpaceException 발생(A)
+ *         copyFiles();
+ *     } catch (SpaceException e) {
+ *         InstallException ie = new InstallException("설치중 공간 부족으로 예외 발생") // 예외 생성(B), A 예외를 생성된 예외에 담아서 반환.
+ *         ie.initCause(e);
+ *         throw ie;
+ *     } catch (MemoryException me) {
+ *         InstallException ie = new InstallException("설치중 메모리 부족으로 예외 발생") // 예외 생성(B), 새로운 예외를 담아서 반환.
+ *         ie.initCause(e);
+ *         throw ie;
+ *         ...
+ *
+ *    - 연결된 예외를 사용하는 이유.
+ *      1. 여러 예외를 하나로 묶어서 다루기 위해서, 즉 캐치블럭을 줄일 수 있기 때문.
+ *      2. checked예외를 unchecked예외로 변경하려 할 때
+ *          ex)
+ *              static void startInstall() throws SpaceException, MemoryException {
+ *                  if(!enoughSpace())
+ *                      throw new SpaceException("설치할 공간이 부족합니다.");
+ *                  if(!enoughMemory())
+ *                      throw new MemoryException("메모리가 부족합니다.");
+ *              }
+ *
+ *              --MemoryException을 unchecked예외로 변경-->
+ *
+ *              static void startInstall() throws SpaceException{
+ *                  if(!enoughSpace())
+ *                      throw new SpaceException("설치할 공간이 부족합니다.");
+ *                  if(!enoughMemory())
+ *                      throw new RuntimeException(new MemoryException("메모리가 부족합니다.")); // RuntimeException을 만들고 그 안에 원인예외로 등록.
+ *              }
  */
-
 
 public class Exception07 {
     public static void main(String[] args){
-
+        System.out.println("연결된 예외를 간단히 설명하면 어떤 예외를 다른 예외로 감싸는 것으로 세부적인 예외들을 포괄적인 예외로 감쌀때와 필수예외처리를 선택예외처리로 변경할 때 사용하곤 합니다.");
     }
 }
