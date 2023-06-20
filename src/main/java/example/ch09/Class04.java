02229d000b5a667f9bb22efac99706b8515bd230
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2023-06-20 17:55:51 +0900

    230620 17:55 regular commit.

---
+++ src/main/java/example/ch09/Class04.java
@@ -0,0 +1,15 @@
+package example.ch09;
+
+public class Class04 {
+    public static void main(String[] args){
+        /**
+         *    - String 클래스의 생성자와 메서드(1/5)
+         *
+         *      String(String s) : 주어진 문자열(s)를 갖는 String 인스턴스를 생성한다.
+         *          ex) String s = new String("Hello");  :  s = "Hello"
+         *      String(char[] value) : 주어진 문자열(value)을 갖는 String인스턴스를 생성한다.
+         *          ex) char[] c = {'H','e','l','l','o'};
+         *              String s = new String(c);   : s = "Hello"
+         */
+    }
+}
