7be1969b8ea8976b4ae80d31e8376757b9c88492
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2023-06-21 21:12:18 +0900

    230621 21:12 The constructor and method of the String class (2/5).

--- src/main/java/example/ch09/Class04.java
+++ src/main/java/example/ch09/Class04.java
@@ -33,5 +33,45 @@ public class Class04 {
         System.out.println(i1);
         System.out.println(i2);
         System.out.println(i3);
+
+        /**
+         *    - String 클래스의 생성자와 메서드(2/5)
+         */
+
+        //String concat(String str) : 문자열(str)을 뒤에 덧붙인다.
+        String s5 = "Hello";
+        String s6 = s5.concat(" World");
+        System.out.println(s6);
+
+        //boolean contains(CharSequence s) : 지정된 문자열(s)이 포함되었는지 검사한다. CharSequence는 인터페이스이다.
+        String s7 = "abcdefg";
+        boolean b = s7.contains("bc");
+        System.out.println(b);
+
+        //boolean endsWith(String suffix) : 지정된 문자열(suffix)로 끝나는지 검사한다.
+        String file = "Hello.txt";
+        boolean b2 = file.endsWith("txt");
+        System.out.println(b2);
+
+        //boolean equals(Object obj) : 매개변수로 받은 문자열(obj)과 String인스턴스의 문자열을 비교한다. obj가 String이 아니거나 문자열이 다르면 false를 반환한다.
+        String s8 = "Hello";
+        boolean b3 = s8.equals("Hello");
+        boolean b4 = s8.equals("Hello");
+        System.out.println(b3);
+        System.out.println(b4);
+
+        //boolean equalsIgnoreCase(String str) : 문자열과 String인스턴스의 문자열을 대소문자 구분없이 비교한다.
+        String s9 = "Hello";
+        boolean b5 = s9.equalsIgnoreCase("HELLO");
+        boolean b6 = s9.equalsIgnoreCase("HeLLo");
+        System.out.println(b5);
+        System.out.println(b6);
+
+        //int indexOf(int ch) : 주어진 문자(ch)가 문자열에 존재하는지 확인하여 위치(index)를 알려준다. 못 찾으면 -1을 반환한다.(index는 0부터 시작)
+        String s10 = "Hello";
+        int idx1 = s10.indexOf('o');
+        int idx2 = s10.indexOf('k');
+        System.out.println(idx1);
+        System.out.println(idx2);
     }
 }
