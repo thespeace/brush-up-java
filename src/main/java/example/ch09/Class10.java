5f25fc088189621fc5f7f81a15645c1563549b57
thespeace <thespeace1@gmail.com>
2023-06-29 18:05:10 +0900

    230629 18:05 The constructor and method of the StringBuffer class (4/4).

--- src/main/java/example/ch09/Class10.java
+++ src/main/java/example/ch09/Class10.java
@@ -23,5 +23,41 @@ public class Class10 {
         //StringBuffer reverse() : StringBuffer인스턴스에 저장되어 있는 문자열의 순서를 거꾸로 나열한다.
         sb.reverse();
         System.out.println("reverse() : "+sb);
+
+        System.out.println("=======================================================================");
+
+
+        /**
+         *    - StringBuffer의 생성자와 메서드(4/4)
+         */
+
+        //void setCharAt(int index, char ch) : 지정된 위치의 문자를 주어진 문자(ch)로 바꾼다.
+        StringBuffer sb1 = new StringBuffer("0123456");
+        sb1.setCharAt(5,'o');
+        System.out.println("setCharAt(int index, char ch) : "+sb1);
+
+        //void setLength(int newLength) : 지정된 길이로 문자열의 길이를 변경한다. 길이를 늘리는 경우에 나머지 빈 공간을 Null문자 '\u0000'로 채운다.
+        StringBuffer sb2 = new StringBuffer("0123456");
+        sb2.setLength(5);
+        System.out.println(sb2);
+
+        StringBuffer sb3 = new StringBuffer("0123456");
+        sb3.setLength(10);
+        String str = sb3.toString().trim();
+        System.out.println("setLength(5) : " + sb2);
+        System.out.println("setLength(10) : " + sb3);
+        System.out.println("toString().trim() : "+ str);
+
+        //String toString() : StringBuffer인스턴스의 문자열을 String으로 반환
+        StringBuffer sb4 = new StringBuffer("0123456");
+        String str1 = sb4.toString();
+        System.out.println("toString() : " + str1);
+
+        //String substring(int start) , String substring(int start, int end) : 지정된 범위 내의 문자열을 String으로 뽑아서 반환한다. 시작위치(start)만 지정하면 시작위치부터 문자열 끝까지 뽑아서 반환한다.
+        String str2 = sb4.substring(3);
+        String str3 = sb4.substring(3, 5);
+        System.out.println("substring(int start) : " + str2);
+        System.out.println("substring(int start, int end) : " + str3);
+
     }
 }
