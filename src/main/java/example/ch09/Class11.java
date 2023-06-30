8306f23e9611e80da85669b346bc4e06c51c73bd
thespeace <thespeace1@gmail.com>
2023-06-30 19:26:35 +0900

    230630 19:25 regular commit.

---
+++ src/main/java/example/ch09/Class11.java
@@ -0,0 +1,13 @@
+package example.ch09;
+
+public class Class11 {
+    public static void main(String[] args){
+        /**
+         *    - StringBuilder
+         *      StringBuffer와 거의 비슷하지만, 딱 하나 다른점으로 Buffer은 동기화가 되어 있어서 멀티 쓰레드에 안전(thread-safe)하지만, Builder은 동기화가 되어있지 않다.
+         *
+         *
+         */
+
+    }
+}
