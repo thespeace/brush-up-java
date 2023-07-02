60a6ac773ec532a0fe937486b131037657bfd4cf
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2023-07-02 23:12:18 +0900

    230702 23:12 Wrapper class, Number class.

--- src/main/java/example/ch09/Class13.java
+++ src/main/java/example/ch09/Class13.java
@@ -54,12 +54,12 @@ public class Class13 {
 
 
         //래퍼 클래스 예제.
-        Integer i = new Integer(100);
-        Integer i2 = new Integer(100);
-        System.out.println("i==i2 ? " + (i == i2)); // 주소 비교.
-        System.out.println("i.equals(i2) ? " + i.equals(i2)); // 값 비교.
-        System.out.println("i.compareTo(i2) ? " + i.compareTo(i2)); //같으면 0, 작으면 양수, 크면 음수 반환.
-        System.out.println("i.toString() ? " + i.toString()); // 문자열 반환.
+        Integer i3 = new Integer(100);
+        Integer i4 = new Integer(100);
+        System.out.println("i==i2 ? " + (i3 == i4)); // 주소 비교.
+        System.out.println("i.equals(i2) ? " + i3.equals(i4)); // 값 비교.
+        System.out.println("i.compareTo(i2) ? " + i3.compareTo(i4)); //같으면 0, 작으면 양수, 크면 음수 반환.
+        System.out.println("i.toString() ? " + i3.toString()); // 문자열 반환.
 
         System.out.println("MAX_VALUE="+Integer.MAX_VALUE); // 최댓값.
         System.out.println("MIN_VALUE="+Integer.MIN_VALUE); // 최소값.
