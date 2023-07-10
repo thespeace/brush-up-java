d3fccdcee99ece67c25ce9b4dff44aba88dbfd28
thespeace <thespeace1@gmail.com>
2023-07-10 16:32:03 +0900

    230710 16:31 Learn SimpleDateFormat(1/2).

---
+++ src/main/java/example/ch10/Datetime08.java
@@ -0,0 +1,34 @@
+package example.ch10;
+
+import java.text.DecimalFormat;
+import java.text.ParseException;
+
+public class Datetime08 {
+
+    /**
+     *    - SimpleDateFormat(1/2)
+     *      날짜와 시간을 다양한 형식으로 출력할 수 있게 해준다.
+     *      {@code
+     *          Date today = new Date();
+     *          SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd"); // 년-월-일
+     *          String result = df.format(today); // 오늘 날짜를 yyyy-MM-dd형태로 변환하여 반환한다.
+     *      }
+     *
+     *    - SimpleDateFormat 패턴
+     *      기호 |          의 미         | 보기
+     *        G |       연대(BC,AD)      | AD
+     *        y |           년도         | 2006
+     *        M |  월(1~12 또는 1월~12월) | 10 또는 10월, OCT
+     *        w |  년의 몇 번째 주(1~53)  | 50
+     *        W |   월의 몇 번째 주(1~5)  | 4
+     *        D |  년의 몇 번째 일(1~366) | 100
+     *        d |  월의 몇 번째 일(1~31)  | 15
+     *        F |  월의 몇 번째 요일(1~5)  | 1
+     *        E |           요일         | 월
+     *
+     *
+     */
+    public static void main(String[] args) throws ParseException {
+
+    }
+}
