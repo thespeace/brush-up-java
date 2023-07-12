372451301f9012a138122f1258a7d82241b51319
thespeace <thespeace1@gmail.com>
2023-07-12 18:13:32 +0900

    230712 18:13 regular commit.

---
+++ src/main/java/example/ch11/Collection01.java
@@ -0,0 +1,19 @@
+package example.ch11;
+
+import java.util.Calendar;
+
+public class Collection01 {
+    public static void main(String[] args){
+        /**
+         *    - 컬렉션 프레임워크(collections framework)
+         *      ** 객체 지향 개념 다음으로 중요, 전체적으로 빠르게 여러번 반복하여 실습하는게 중요하다. **
+         *
+         *      컬렉션(collection) : 여러 객체(데이터)를 모아 놓은 것을 의미.
+         *      프레임워크(framework) : 표준화, 정형화된 체계적인 프로그래밍 방식. 라이브러리 + 프로그래밍 방식.(자유도가 떨어지지만 프로그래밍 생산성 증가, 유지보수가 수월해진다.)
+         *                            + 라이브러리(library) : 프로그래밍에서는 다른 사람들이 미리 유용한 기능을 만들어 모아 놓은 것을 의미. ex) 통계, 그래픽, 오디오 라이브러리.
+         *      {@code
+         *
+         *      }
+         */
+    }
+}
