56edbfeb64c099d804543f3e21655034ead4e470
thespeace <thespeace1@gmail.com>
2023-07-17 18:02:22 +0900

    230717 18:02 regular commit.

---
+++ src/main/java/example/ch11/Collection05.java
@@ -0,0 +1,17 @@
+package example.ch11;
+
+public class Collection05 {
+    public static void main(String[] args) {
+        /**
+         *    - 스택과 큐(Stack & Queue)
+         *      1. 스택(Stack) : LIFO(Last In First Out)구조, 마지막에 저장된 것을 제일 먼저 꺼내게 된다. ex) 밑이 막힌 Box.
+         *                      저장(push), 추출(pop)의 순서가 반대이다.
+         *                      위로만 넣고 뺄 수 있다.
+         *
+         *      2. 큐(Queue) : FIFO구조, 제일 먼저 저장한 것을 제일 먼저 꺼내게 된다. ex) 줄서기, 양 끝이 뚫린 상자.
+         *                    저장(offer), 추출(poll)
+         *
+         */
+
+    }
+}
