e272dfcb8d56ab61e91791b3a0d9d961dfee6881
DESKTOP-E63NM7V\SSH <thespeace1@gmail.com>
2023-07-29 23:34:21 +0900

    230729 23:34 HashSet(2/2).

--- src/main/java/example/ch11/Collection11.java
+++ src/main/java/example/ch11/Collection11.java
@@ -43,14 +43,97 @@ public class Collection11 {
          *      }
          */
 
-
-
-
-
-
-
-
         System.out.println("==============예제 3=============");
 
+        HashSet set = new HashSet();
+
+        set.add("abc");
+        set.add("abc"); // 중복이라 저장안됨.
+        set.add(new Person("David",10));
+        set.add(new Person("David",10));
+
+        System.out.println(set);
+
+
+        System.out.println("==============예제 4=============");
+        HashSet setA   = new HashSet();
+        HashSet setB   = new HashSet();
+        HashSet setKyo = new HashSet();
+        HashSet setHab = new HashSet();
+        HashSet setCha = new HashSet();
+
+        setA.add("1");	 setA.add("2");  setA.add("3");
+        setA.add("4");  setA.add("5");
+        System.out.println("A = "+setA);
+
+        setB.add("4");	 setB.add("5");  setB.add("6");
+        setB.add("7");  setB.add("8");
+        System.out.println("B = "+setB);
+
+        Iterator it = setB.iterator();
+
+        //교집합 구하기.
+        while(it.hasNext()) {
+            Object tmp = it.next();
+            if(setA.contains(tmp))
+                setKyo.add(tmp);
+        }
+//        setA.retainAll(setB); //교집합, 공통된 요소만 남기고 setA에  삭제.
+//        setKyo.addAll(setA);
+
+        //합집합 구하기.
+        it = setA.iterator();
+        while(it.hasNext())
+            setHab.add(it.next());
+
+        it = setB.iterator();
+        while(it.hasNext())
+            setHab.add(it.next());
+//        setA.addAll(setB); // 합집합, setB의 모든 요소를 setA에 추가(중복 제외)
+//        System.out.println(setA);
+
+
+        //차집합 구하기.
+        it = setA.iterator();
+        while(it.hasNext()) {
+            Object tmp = it.next();
+            if(!setB.contains(tmp))
+                setCha.add(tmp);
+        }
+//        setA.removeAll(setB); // 차집합, setB와 공통 요소를 제거하고 setA에 저장.
+//        System.out.println(setA);
+
+        System.out.println("A ∩ B = " + setKyo);  // 한글 ㄷ을 누르고 한자키
+        System.out.println("A U B = " + setHab);  // 한글 ㄷ을 누르고 한자키
+        System.out.println("A - B = " + setCha);
+
+    }
+}
+
+//equals()와 hashCode()를 오버라이딩해야 hashSet이 올바르게 동작.(중복 제거 ...)
+class Person {
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String toString() {
+        return name +":"+ age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Person)) return false;
+        Person p = (Person)o;
+        //나자신(this)의 이름과 나이를 p와 비교.
+        return this.name.equals(p.name) && this.age == p.age;
+    }
+    @Override
+    public int hashCode() {
+        //int hash(Object... values); //가변인자 : 매개변수의 제한이 없다.
+        return Objects.hash(name, age);
     }
 }
