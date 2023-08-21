83b2f181d5ae505e1645d00a8b49087e0f832e08
thespeace <thespeace1@gmail.com>
2023-08-21 22:11:57 +0900

    230821 22:11 regular commit.

---
+++ src/main/java/example/ch13/Thread01.java
@@ -0,0 +1,15 @@
+package example.ch13;
+
+public class Thread01 {
+    public static void main(String[] args){
+        /**
+         *    - 프로세스와 쓰레드(process & thread)
+         *
+         *      프로세스 : 실행 중인 프로그램, 자원(resource : 메모리, cpu, 다른 컴퓨터 디바이스 ...)과 쓰레드로 구성.
+         *      쓰레드  : 프로세스 내에서 실제 작업을 수행.
+         *               모든 프로세스는 최소한 하나의 쓰레드를 가지고 있다.
+         *
+         *                  프로세스 : 쓰레드 = 공장(작업환경) : 일꾼(작업자)
+         */
+    }
+}
