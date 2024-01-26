aabf3898c19da93f470c24b953bfc827ec369f4b
thespeace <thespeace1@gmail.com>
2024-01-26 22:19:52 +0900

    FEAT: Example-payment system, user input

--- src/main/java/example/ch17/practice/p11_poly/intermediate/ex10/PayMain.java
+++ src/main/java/example/ch17/practice/p11_poly/intermediate/ex10/PayMain.java
@@ -1,10 +1,29 @@
 package example.ch17.practice.p11_poly.intermediate.ex10;
 
+import java.util.Scanner;
+
 //결제 시스템
 public class PayMain {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         PayService payService = new PayService();
 
+        while(true) {
+            System.out.print("결제 수단을 입력하세요 : ");    //만일, 결제수단을 추가하길 원한다면 PayStore 코드만 변경해주면 된다.
+            String payOption = scanner.nextLine();
+
+            if(payOption.equals("exit")){
+                System.out.println("프로그램을 종료합니다.");
+                return;
+            }
+
+            System.out.print("결제 금액을 입력하세요 : ");
+            int amount = scanner.nextInt();
+            scanner.nextLine();
+            payService.processPay(payOption, amount);
+        }
+
+/*
         //kakao 결제
         String payOption1 = "kakao";
         int amount1 = 5000;
@@ -24,6 +43,8 @@ public class PayMain {
         String payOption4 = "new";
         int amount4 = 20000;
         payService.processPay(payOption4, amount4);
+*/
+
     }
 }
 
